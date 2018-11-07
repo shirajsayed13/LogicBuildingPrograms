ef067eae7071ffbe4413f5a8617358486ff66efb
Shiraj Sayed <shirajsayed13@gmail.com>
2018-11-07 17:09:43 +0530

    Added addNumbers Program

--- src/com/upgradme/Main.java
+++ src/com/upgradme/Main.java
@@ -1,12 +1,22 @@
 package com.upgradme;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
         main.pattern();
+        main.addNumbers();
     }
 
+    /**
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+     **/
     private void pattern() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
@@ -14,5 +24,19 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    //Add Numbers
+    private void addNumbers() {
+        int x, y, z;
+        System.out.println("Enter two Integers to calculate their Sum ");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter value of X ");
+        x = in.nextInt();
+        System.out.println("Enter value for Y ");
+        y = in.nextInt();
+
+        z = x + y;
+        System.out.println("Sum of Entered Integers = " + z);
     }
 }
